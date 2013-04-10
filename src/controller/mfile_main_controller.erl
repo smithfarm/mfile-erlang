889c831294110e72077559dd9f7116add719dde4
smithfarm <presnypreklad@gmail.com>
2013-04-10 14:35:28 +0200

    Once again committing non-working version before checkout

--- src/controller/mfile_main_controller.erl
+++ src/controller/mfile_main_controller.erl
@@ -1,6 +1,6 @@
 -module(mfile_main_controller, [Req]).
 -compile(export_all).
--compile([{parse_transform, lager_transform}]).
+%-compile([{parse_transform, lager_transform}]).
 
 
 initializeForm() ->
@@ -13,94 +13,98 @@ initializeForm() ->
 
 % GET /
 start('GET', []) ->
-   %lager:start(),
+%   lager:start(),
    {ok, initializeForm() }.
 
 % insert record (called asynchronously using AJAX)
 insert('POST', []) ->
-   MfileRec = mfile:new( id, 
-                         calendar:now_to_datetime(erlang:now()), 
-			 0,
-			 0, 
-			 Req:post_param("mfileKeyw"), 
-			 Req:post_param("mfileDesc") 
-                       ),
-   {ok,{mfile,Id,TmpTime,CId,Sern,Keyw,FileDesc}} = MfileRec:save(),
-   % The DB model sends back a timestamp in the format {{Y, M, D}, {H, M, S}} 
-   % Now convert this into a binary of the format <<"YYYY-MMM-DD">> for display on-screen
-   DateStr = mfilelib:timestamp_to_binary_date_only(TmpTime),
-   {json, { queryResult, "success" },
-          [ {mfileId,   Id}, 
-   	    {mfileDate, DateStr},
-            {mfileCodeId, CId},
-            {mfileCode, ""},
-	    {mfileSern, Sern},
-   	    {mfileKeyw, Keyw}, 
-	    {mfileDesc, FileDesc} ] }.
+%   MfileRec = mfile:new( id, 
+%                         calendar:now_to_datetime(erlang:now()), 
+%			 0,
+%			 0, 
+%			 Req:post_param("mfileKeyw"), 
+%			 Req:post_param("mfileDesc") 
+%                       ),
+%   {ok,{mfile,Id,TmpTime,CId,Sern,Keyw,FileDesc}} = MfileRec:save(),
+%   % The DB model sends back a timestamp in the format {{Y, M, D}, {H, M, S}} 
+%   % Now convert this into a binary of the format <<"YYYY-MMM-DD">> for display on-screen
+%   DateStr = mfilelib:timestamp_to_binary_date_only(TmpTime),
+%   {json, { queryResult, "success" },
+%          [ {mfileId,   Id}, 
+%   	    {mfileDate, DateStr},
+%           {mfileCodeId, CId},
+%           {mfileCode, ""},
+%	    {mfileSern, Sern},
+%   	    {mfileKeyw, Keyw}, 
+%	    {mfileDesc, FileDesc} ] }.
+   true.
 
 % insertcode helper function - sends Code entered by the user to DB
-gen_insertcode_JSON(GicJCode) ->
-   MfilecodeRec = mfilecode:new( id,
-                                 calendar:now_to_datetime(erlang:now()),
-				 lists:map(fun mfilelib:uppercase_it/1, GicJCode),
-				 "success"
-                               ),
-   {ok,{mfilecode,Id,CodeTmpTime,Code,CodeDesc}} = MfilecodeRec:save(),
-   % The DB model sends back a timestamp in the format {{Y, M, D}, {H, M, S}} 
-   % Now convert this into a binary of the format <<"YYYY-MMM-DD">> for display on-screen
-   CodeDateStr = mfilelib:timestamp_to_binary_date_only(CodeTmpTime),
-   {json, { queryResult, "success" },
-          [ {mfilecodeId,   Id},
-            {mfilecodeDate, CodeDateStr},
-	    {mfilecodeCode, Code},
-	    {mfilecodeDesc, CodeDesc} ] }.
+%gen_insertcode_JSON(GicJCode) ->
+%   MfilecodeRec = mfilecode:new( id,
+%                                 calendar:now_to_datetime(erlang:now()),
+%				 lists:map(fun mfilelib:uppercase_it/1, GicJCode),
+%				 "success"
+%                               ),
+%   {ok,{mfilecode,Id,CodeTmpTime,Code,CodeDesc}} = MfilecodeRec:save(),
+%   % The DB model sends back a timestamp in the format {{Y, M, D}, {H, M, S}} 
+%   % Now convert this into a binary of the format <<"YYYY-MMM-DD">> for display on-screen
+%   CodeDateStr = mfilelib:timestamp_to_binary_date_only(CodeTmpTime),
+%   {json, { queryResult, "success" },
+%          [ {mfilecodeId,   Id},
+%            {mfilecodeDate, CodeDateStr},
+%	    {mfilecodeCode, Code},
+%	    {mfilecodeDesc, CodeDesc} ] }.
 
 % insert code (called asynchronously using AJAX)
 insertcode('POST', [])->
-   C = Req:post_param("mfilecodeCode"), 
-   case mfilelib:validate_mfilecode(C) of
-   	true           -> gen_insertcode_JSON(C);
-	false          -> { json, { queryResult, "Upper and lower case ASCII letters only." }, [] };
-	{error, IcErr} -> { json, { queryResult, IcErr }, [] }
-   end.
+%   C = Req:post_param("mfilecodeCode"), 
+%   case mfilelib:validate_mfilecode(C) of
+%   	true           -> gen_insertcode_JSON(C);
+%	false          -> { json, { queryResult, "Upper and lower case ASCII letters only." }, [] };
+%	{error, IcErr} -> { json, { queryResult, IcErr }, [] }
+%   end.
+    true.
 
 % fetch record by Code and Serial Number (called asynchronously using AJAX)
 fetch('POST', []) ->
-   Cf = Req:post_param("mfileCode"),  % get Code string from form
-   Sf = Req:post_param("mfileSern"),  % get Serial Number from form
-
-   Result = mfilelib:validate_codestr_and_sern(Cf, Sf),
-   %lager:info("validate_codestr_and_sern returned ~p", [Result]),
-   { {result, R}, {codeid, I}, 
-     {codestr, C}, {sern, S} } = Result,
-   % if the Code exists in the database, I will be something other than 0
-   Json = if
-       (I /= 0) and (S /= 0) -> 
-           case boss_db:find(mfile, [{code_id, 'equals', I}, {sern, 'equals', S}]) of
-           	[{mfile,V1,V2,V3,V4,V5,V6}] -> {json, {queryResult, "success"},
-		                                      [ {mfileId,   V1}, 
-                                                        {mfileDate, V2},
-                                                        {mfileCode, V3},
-                                                        {mfileSern, V4},
-                                                        {mfileKeyw, V5}, 
-                                                        {mfileDesc, V6} ]};
-        	[] -> {json, { queryResult, lists:append(["Code '", C, "' not found"]) }, [] }
-           end;
-
-       not ((I /= 0) and (S /= 0)) -> {json, { queryResult, R }, [] }
-   end,
-   %lager:info("sending back JSON: ~p", [Json]),
-   Json.
+%   Cf = Req:post_param("mfileCode"),  % get Code string from form
+%   Sf = Req:post_param("mfileSern"),  % get Serial Number from form
+%
+%   Result = mfilelib:validate_codestr_and_sern(Cf, Sf),
+%   %lager:info("validate_codestr_and_sern returned ~p", [Result]),
+%   { {result, R}, {codeid, I}, 
+%     {codestr, C}, {sern, S} } = Result,
+%   % if the Code exists in the database, I will be something other than 0
+%   Json = if
+%       (I /= 0) and (S /= 0) -> 
+%           case boss_db:find(mfile, [{code_id, 'equals', I}, {sern, 'equals', S}]) of
+%           	[{mfile,V1,V2,V3,V4,V5,V6}] -> {json, {queryResult, "success"},
+%		                                      [ {mfileId,   V1}, 
+%                                                        {mfileDate, V2},
+%                                                        {mfileCode, V3},
+%                                                        {mfileSern, V4},
+%                                                        {mfileKeyw, V5}, 
+%                                                        {mfileDesc, V6} ]};
+%        	[] -> {json, { queryResult, lists:append(["Code '", C, "' not found"]) }, [] }
+%           end;
+%
+%       not ((I /= 0) and (S /= 0)) -> {json, { queryResult, R }, [] }
+%   end,
+%   %lager:info("sending back JSON: ~p", [Json]),
+%   Json.
+    true.
 
 % fetch code (called asynchronously using AJAX)
 fetchcode('POST', []) ->
-   X = Req:post_param("mfilecodeCode"),
-   {QR, V1, V2, V3, V4} = mfilelib:fetch_code(X),
-   {json, {queryResult, QR},
-          {mfilecodeId, V1},
-          {mfilecodeDate, V2},
-          {mfilecodeCode, V3},
-          {mfilecodeDesc, V4} }.
+%   X = Req:post_param("mfilecodeCode"),
+%   {QR, V1, V2, V3, V4} = mfilelib:fetch_code(X),
+%   {json, {queryResult, QR},
+%          {mfilecodeId, V1},
+%          {mfilecodeDate, V2},
+%          {mfilecodeCode, V3},
+%          {mfilecodeDesc, V4} }.
+    true.
    
 % error handler
 lost('GET', []) ->
