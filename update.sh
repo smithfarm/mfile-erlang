e06381864b9a59fc140c33a5c56160c1ef5bc7d3
smithfarm <presnypreklad@gmail.com>
2013-04-11 10:47:43 +0200

    Corrected yet another glitch in mfile-release

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ echo "Changelog entry for this update (ALL SINGLE-QUOTES MUST BE DOUBLED UP!!!!)
 read CHGLOGENTRY
 
 # update Changelog
-declare CMD="sed '1 i `date +%Y-%m-%d` Version $VERNUM $CHGLOGENTRY' Changelog >Changelog.new"
+CMD="sed -e \"1 i `date +%Y-%m-%d` Version $VERNUM $CHGLOGENTRY\" -e \"s/'/''/g\" Changelog >Changelog.new"
 eval $CMD
 mv Changelog.new Changelog
 
